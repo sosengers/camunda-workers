b7370b5ad8af3faea3bd31e97e5788784d10e437
Marco Ferrati <marco.ferrati@gmail.com>
2021-02-21 15:56:07 +0100

    Implement external worker send_result

--- workers/register_user_interest/send_result.py
+++ workers/register_user_interest/send_result.py
@@ -1,5 +1,24 @@
 from camunda.external_task.external_task import ExternalTask, TaskResult
+import json
+import pika
 
 def send_result(task: ExternalTask) -> TaskResult:
+
+    operation_result = task.get_variable("operation_result")
+    interest = json.loads(task.get_variable("interest"))
+    pg_username = interest.get("prontogram_username")
+
+    connection = pika.BlockingConnection(pika.ConnectionParameters('acmesky_mq'))
+    channel = connection.channel()
+
+    channel.queue_declare(queue=pg_username, durable=True)
+
+    channel.basic_publish(exchange='',
+                      routing_key=pg_username,
+                      body=bytes(operation_result, 'utf-8'),
+                      properties=pika.BasicProperties(delivery_mode=2)
+                      )
+
+    connection.close()
 
     return task.complete()
