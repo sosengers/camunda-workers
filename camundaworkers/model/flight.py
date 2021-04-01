ef55889a1cb11ab4d99a63ce79a2b331c601e9fc
Tommaso Azzalin <azzalintommaso@gmail.com>
2021-04-01 12:47:58 +0200

    Task verify-offer-code-validity, rehabilitation-offer-code

--- camundaworkers/model/flight.py
+++ camundaworkers/model/flight.py
@@ -1,5 +1,4 @@
-from sqlalchemy import Column, String, DateTime, Float, Boolean, UniqueConstraint, Integer, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String, DateTime, Float, UniqueConstraint, Integer
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -32,17 +31,3 @@ class Flight(Base):
     __table_args__ = (
         UniqueConstraint('flight_id', 'departure_datetime'),
     )
-
-
-class OfferMatch(Base):
-    __tablename__ = 'offermatches'
-
-    offer_code = Column(String(11), primary_key=True)
-
-    outbound_flight_id = Column(Integer, ForeignKey("flights.id"))
-    outbound_flight = relationship("Flight", primaryjoin=outbound_flight_id == Flight.id)
-
-    comeback_flight_id = Column(Integer, ForeignKey("flights.id"))
-    comeback_flight = relationship("Flight", primaryjoin=comeback_flight_id == Flight.id)
-
-    blocked = Column(Boolean, default=False)
--- camundaworkers/workers/buy_offer/rehabilitation_offer_code.py
+++ camundaworkers/workers/buy_offer/rehabilitation_offer_code.py
@@ -1,7 +1,26 @@
 from camunda.external_task.external_task import ExternalTask, TaskResult
 from camundaworkers.logger import get_logger
+from sqlalchemy.orm.session import sessionmaker
+from camundaworkers.logger import get_logger
+from camundaworkers.model.base import create_sql_engine
+from camundaworkers.model.offermatch import OfferMatch
 
 
 def rehabilitation_offer_code(task: ExternalTask) -> TaskResult:
     logger = get_logger()
     logger.info("rehabilitation_offer_code")
+
+    offer_code = str(task.get_variable("offer_code"))
+
+    Session = sessionmaker(bind=create_sql_engine())
+    session = Session()
+
+    affected_rows = session.query(OfferMatch).filter(OfferMatch.offer_code == offer_code).update({"blocked": False}, synchronize_session="fetch")
+    if affected_rows < 1:
+        session.rollback() # TODO verificare se è necessario
+        logger.error(f"{affected_rows} matches were found for the given offer code. The offer code will not be rehabilitated.")
+        return task.complete()
+
+    logger.info(f"{affected_rows} match was found for the given offer code.")
+    session.commit()
+    return task.complete()
