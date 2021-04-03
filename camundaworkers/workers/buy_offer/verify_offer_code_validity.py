c55dfd0fc3361686ac46d7a8ef109592d5bf963b
Tommaso Azzalin <azzalintommaso@gmail.com>
2021-04-03 11:43:06 +0200

    Fix

--- camundaworkers/workers/buy_offer/verify_offer_code_validity.py
+++ camundaworkers/workers/buy_offer/verify_offer_code_validity.py
@@ -19,12 +19,13 @@ def verify_offer_code_validity(task: ExternalTask) -> TaskResult:
     Session = sessionmaker(bind=create_sql_engine())
     session = Session()
 
+    user_communication_code = str(hash(offer_purchase_data))
     affected_rows = session.query(OfferMatch).filter(OfferMatch.offer_code == offer_code).update({"blocked": True}, synchronize_session="fetch")
     if affected_rows < 1:
         session.rollback() # TODO verificare se è necessario
         logger.error(f"{affected_rows} matches were found for the given offer code.")
-        return task.complete(global_variables={'offer_code_validity': False, 'user_communication_code': hash(offer_purchase_data)})
+        return task.complete(global_variables={'offer_code_validity': False, 'user_communication_code': user_communication_code})
 
     logger.info(f"{affected_rows} match was found for the given offer code.")
     session.commit()
-    return task.complete(global_variables={'offer_code_validity': True, 'user_communication_code': hash(offer_purchase_data)})
+    return task.complete(global_variables={'offer_code_validity': True, 'user_communication_code': user_communication_code})
