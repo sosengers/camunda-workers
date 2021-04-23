6c2fabff150d6c9cdf107dae30b3f5e34a51e3dc
TommasoAzz <azzalintommaso@gmail.com>
2021-04-23 11:01:23 +0200

    Aggiornate docs in camundaworkers/workers/register_user_interest

--- camundaworkers/workers/register_user_interest/send_result.py
+++ camundaworkers/workers/register_user_interest/send_result.py
@@ -7,7 +7,7 @@ from camundaworkers.logger import get_logger
 
 def send_result(task: ExternalTask) -> TaskResult:
     """
-    Send through RabbitMQ the result message to the client
+    Sends the result message to the client through RabbitMQ.
     :param task: the current task instance
     :return: the task result
     """
@@ -18,17 +18,14 @@ def send_result(task: ExternalTask) -> TaskResult:
     interest = json.loads(task.get_variable("interest"))
     pg_username = interest.get("prontogram_username")
 
-    """ Connect to RabbitMQ
-    """
+    # Connecting to RabbitMQ
     connection = pika.BlockingConnection(pika.ConnectionParameters('acmesky_mq'))
     channel = connection.channel()
 
-    """ Declare which queue will be used
-    """
+    # Declaring which queue will be used
     channel.queue_declare(queue=pg_username, durable=True)
 
-    """ Publish a message on the queue and close the connection
-    """
+    # Publishing a message on the queue and closing the connection
     channel.basic_publish(exchange='',
                           routing_key=pg_username,
                           body=bytes(operation_result, 'utf-8'),
